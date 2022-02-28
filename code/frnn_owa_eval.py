6fa9fd264b90fd2f631bf65e067ce8e9476f3413
Olha Kaminska <dil-delada@ukr.net>
2022-02-28 01:27:03 +0100

    Upload updated code

--- code/frnn_owa_eval.py
+++ code/frnn_owa_eval.py
@@ -1,7 +1,7 @@
 from frlearn.neighbours import FRNN, KDTree, NNSearch
 from frlearn.utils.owa_operators import OWAOperator, additive, strict, exponential, mean, invadd
 import numpy as np
-
+from scipy.stats import pearsonr
 
 def least_indices_and_values(a, m, axis=-1):
     '''
@@ -17,170 +17,172 @@ def least_indices_and_values(a, m, axis=-1):
     a = np.take_along_axis(a, i_sort, axis=axis)
     return ia, a
 
-
-class Cosine(NNSearch):
+class Cosine(NNSearch):    
     '''
     This class defines the cosine similarity relation that can be used into FRNN OWA classification method
-    Cosine metric: cos(A, B) = (A * B)(||A|| x ||B||),
+    
+    Cosine metric: cos(A, B) = (A * B)(||A|| x ||B||), 
                     where A, B - embedding vectors of tweets, * is a scalar product, and ||.|| is the vector norm
     Cosine similarity: cos_similarity(A, B) = (1 + cos(A, B))/2.
     '''
     class Model(NNSearch.Model):
         X_T: np.ndarray
         X_T_norm: np.ndarray
-            
         def _query(self, X, m_int: int):
             distances = 1 - 0.5 * X @ self.X_T / np.linalg.norm(X, axis=1)[:, None] / self.X_T_norm
             return least_indices_and_values(distances, m_int, axis=-1)
-        
     def construct(self, X) -> Model:
         model: NNSearch.Model = super().construct(X)
         model.X_T = np.transpose(X)
         model.X_T_norm = np.linalg.norm(model.X_T, axis=0)
         return model
 
-
 def frnn_owa_method(train_data, y, test_data, vector_name, NNeighbours, lower, upper):
     '''
-    This function implements FRNN OWA classification method
-    Input:  train_data - train data in form of pandas DataFrame
-            y - the list of train_data golden labels
-            test_data - train data in form of pandas DataFrame
+    This function implements FRNN OWA classification method 
+    
+    Input:  train_data - train data in form of pandas DataFrame 
+            y - the list of train_data golden labels 
+            test_data - train data in form of pandas DataFrame 
             vector_name - string, the name of vector with features in train_data and test_data
             NNeighbours - the int number of neighbours for FRNN OWA method
-            lower, upper - lower and upper approximations, calculated with Ordered Weighted Average (OWA)
-                aggregation operators possible options: strict(), exponential(), invadd(), mean(), additive()
+            lower, upper - lower and upper approximations, calculated with Ordered Weighted Average (OWA) aggregation operators
+                            possible options: strict(), exponential(), invadd(), mean(), additive()
     Output: conf_scores - the list of confidence scores, y_pred - the list of predicted labels
     '''
-    def create_X(data, vector_name):
-        # Helper function to create matrix X for train and test data
-        indexes = list(data.index)
-        vector_size = len(data[vector_name][indexes[0]])
-        X = np.zeros((len(data), vector_size))
-        for k in range(len(indexes)):
-            for j in range(vector_size):
-                X[k][j] = data[vector_name][indexes[k]][j]
-        return X
-
-    X_train = create_X(train_data, vector_name)
-    X_test = create_X(test_data, vector_name)
+    train_ind = list(train_data.index)
+    test_ind = list(test_data.index)
+    X = np.zeros((len(train_data), len(train_data[vector_name][train_ind[0]])))
+    for m in range(len(train_ind)):
+        for j in range(len(train_data[vector_name][train_ind[m]])):
+            X[m][j] = train_data[vector_name][train_ind[m]][j]
+    X_test = np.zeros((len(test_data), len(test_data[vector_name][test_ind[0]])))
+    for k in range(len(test_data)):
+        for j in range(len(test_data[vector_name][test_ind[0]])):
+            X_test[k][j] = test_data[vector_name][test_ind[k]][j]
     OWA = OWAOperator(NNeighbours)
     nn_search = Cosine()
     clf = FRNN(nn_search=nn_search, upper_weights=upper, lower_weights=lower, lower_k=NNeighbours, upper_k=NNeighbours)
-    cl = clf.construct(X_train, y)
+    cl = clf.construct(X, y)     
     # confidence scores
     conf_scores = cl.query(X_test)
-    # labels
+    # labels 
     y_pred = np.argmax(conf_scores, axis=1)
     return conf_scores, y_pred
-
-
-def weights_sum_test(conf_scores, alpha, classes = 4):
-    '''
-    This function performs rescaling and softmax transformation of confidence scores
-    Input:  conf_scores - the list of confidence scores
-            alpha - the int parameter used for confidence scores rescaling, by default it is equal to 0.8
-            classes - the amount of classes for which confidence scores where calculated
-    Output: the list of transformed confidence scores
-    '''
-    conf_scores_T = conf_scores.T
-    conf_scores_T_rescale = [[(conf_scores_T[k][i]-0.5)/(alpha)
-                              for i in range(len(conf_scores_T[k]))] for k in range(classes)]
-    conf_scores_T_rescale_sum = [sum(conf_scores_T_rescale[k]) for k in range(classes)]
-    res = [np.exp(conf_scores_T_rescale_sum[k])/sum([np.exp(conf_scores_T_rescale_sum[m])
-                                                     for m in range(classes)]) for k in range(classes)]
-    return res
-
-
-def test_ensemble_confscores(train_data, y, test_data, vector_names, NNeighbours, lower, upper, alpha):
-    '''
-    This function performs ensemble of FRNN OWA methods based on confidence scores outputs
-    Input:  train_data - pandas DataFrame with train data that contains features 'vector_names'
-            y - pandas Series or list, golden labels of train_data instances
-            test_data - pandas DataFrame with test data that contains features 'vector_names'
-            vector_names - the list of strings, names of features vectors in train_data and test_data
-            NNeighbours - the list of int numbers, where each number is amount of neighbours 'k' that will be used
-                to perform FRNN OWA classification method for the corresponded feature vector.
-                Lenghts of 'vector_names' and 'NNeighbours' lists should be equal.
-            lower, upper - lower and upper approximations, calculated with Ordered Weighted Average (OWA)
-                aggregation operators with possible options: strict(), exponential(), invadd(), mean(), additive()
-            alpha - the int parameter used for confidence scores rescaling, by default it is equal to 0.8
-    Output: y_pred_res - the list of predicted labels
-    '''
-    # Create and fill 3D array with dimentions: number of vectors, number of test instances, number of classes
-    conf_scores_all = np.zeros((len(vector_names), len(test_data), 4))
-    for j in range(len(vector_names)):
-        # Calculate confidence scores for each feature vector
-        result = frnn_owa_method(train_data, y, test_data, vector_names[j], NNeighbours[j], lower, upper)[0]
-        # Check for NaNs
-        for k in range(len(result)):
-            if np.any(np.isnan(result[k])):
-                result[k] = [0, 0, 0, 0]
-        conf_scores_all[j] = (result)
-    # Rescale obtained confidence scores
-    rescaled_conf_scores = np.array([weights_sum_test(conf_scores_all[:, k, :], alpha) 
-                                     for k in range(len(conf_scores_all[0]))])
-    # Use the mean voting function to obtain the predicted label
-    y_pred_res = [np.round(6*np.average(k, weights=[0, 1, 2, 3])) for k in rescaled_conf_scores]
-    return y_pred_res
-
-
-def test_ensemble_labels(train_data, y, test_data, vector_names, NNeighbours, lower, upper):
+    
+def test_ensemble(train_data, y, test_data, vector_names, NNeighbours, lower, upper):
     '''
     This function performs ensemble of FRNN OWA methods based on labels as output
+    
     Input:  train_data - pandas DataFrame with train data that contains features 'vector_names'
-            y - pandas Series or list, golden labels of train_data instances
+            y - pandas Series or list, golden labels of train_data instances 
             test_data - pandas DataFrame with test data that contains features 'vector_names'
             vector_names - the list of strings, names of features vectors in train_data and test_data
-            NNeighbours - the list of int numbers, where each number represent amount of neighbours 'k' that will
-                be used to perform FRNN OWA classification method for the corresponded feature vector.
-                Lenghts of 'vector_names' and 'NNeighbours' lists should be equal. 
-            lower, upper - lower and upper approximations, calculated with Ordered Weighted Average (OWA)
-                aggregation operators with possible options: strict(), exponential(), invadd(), mean(), additive()
+            NNeighbours - the list of int numbers, where each number represent amount of neighbours 'k' that will be used to perform    
+                           FRNN OWA classification method for the corresponded feature vector. Lenghts of 'vector_names' and 'NNeighbours' lists should be equal. 
+            lower, upper - lower and upper approximations, calculated with Ordered Weighted Average (OWA) aggregation operators
+                            possible options: strict(), exponential(), invadd(), mean(), additive()
     Output: y_pred_res - the list of predicted labels
     '''
     y_pred = []
-    for j in range(len(vector_names)):
+    for j in range(len(vector_names)):       
         y_pred.append(frnn_owa_method(train_data, y, test_data, vector_names[j], NNeighbours[j], lower, upper)[1])
-    # Use voting function to obtain the ensembled label - we used mean
+    # Use voting function to obtain the ensembled label - we used mean 
     y_pred_res = np.mean(y_pred, axis=0)
     return y_pred_res
 
-
-def cross_validation_ensemble_owa(df, vector_names, K_fold, NNeighbours, lower, upper, method, alpha=0.8):
+def cross_validation_ensemble_owa(df, vector_names, class_name, K_fold, NNeighbours, lower, upper):
     '''
-    This function performs cross-validation evaluation for FRNN OWA ensemble
-    Input:  df - pandas DataFrame with features to evaluate
+    This function performs cross-validation evaluation for FRNN OWA ensemble 
+    
+    Input:  df - pandas DataFrame with features to evaluate 
             vector_names - the list of strings, names of features vectors in df
+            class_name - name of the column in df that corresponds to labels 
             K_fold - the number of folds of cross-validation, we used K_fold = 5
-            NNeighbours - the list of int numbers, where each number represent amount of neighbours 'k' that
-                will be used to perform FRNN OWA classification method for the corresponded feature vector.
-                Lenghts of 'vector_names' and 'NNeighbours' lists should be equal.
-            lower, upper - lower and upper approximations, calculated with Ordered Weighted Average (OWA)
-                aggregation operators with possible options: strict(), exponential(), invadd(), mean(), additive()
-            method - this string variable defines the output of FRNN OWA approach, it can be 'labels' or 'conf_scores'
-            alpha - the int parameter used for confidence scores rescaling, by default it is equal to 0.8
-    Output: Pearson Correlation Coefficient (PCC) as float number
-            PCC = (sum_i(x_i-mean(x))(y_i-mean(y)))/(sqrt(sum_i(x_i-mean(x))^2*sum_i(y_i-mean(y))^2)),
-            where x_i and y_i present the i-th components of vectors x and y
+            NNeighbours - the list of int numbers, where each number represent amount of neighbours 'k' that will be used to 
+                perform FRNN OWA classification method for the corresponded feature vector. Lenghts of 'vector_names' and 'NNeighbours' lists should be equal. 
+            lower, upper - lower and upper approximations, calculated with Ordered Weighted Average (OWA) aggregation operators
+                            possible options: strict(), exponential(), invadd(), mean(), additive()
+    Output: df as pandas DataFrame, identical to input df but with additional column 'Labels' that contains predictions
     '''
-    pearson = []
+    df['Labels'] = None
     random_indices = np.random.permutation(df.index)
-    for i in range(K_fold):
+    for i in range(K_fold): 
         # Split df on train and test data
         test_data = df.loc[df.index.isin(random_indices[i*len(df.index)//K_fold:(i+1)*len(df.index)//K_fold])]
         train_data = df[~df.index.isin(test_data.index)]
-        y = train_data['Class']
-        y_true = test_data['Class']
-        # Apply FRNN OWA method for each feature vector depends on specified output type
-        if method == 'labels':
-            # Solution for labels calculation
-            y_pred_res = test_ensemble_labels(train_data, y, test_data, vector_names, NNeighbours, lower, upper)
-        elif method == 'conf_scores':
-            # Solution for confidence scores calculation
-            y_pred_res = test_ensemble_confscores(train_data, y, test_data, vector_names, NNeighbours, lower, upper, alpha)
-        else:
-            print('Wrong output type was specified!')
-        # Calculate PCC
-        pearson.append(pearsonr(y_true, y_pred_res)[0])
-    return np.mean(pearson)
+        y = train_data[class_name]    
+        # Apply FRNN OWA method 
+        y_pred_res = test_ensemble(train_data, y, test_data, vector_names, NNeighbours, lower, upper)
+        df['Labels'].loc[test_data.index] = y_pred_res
+    return df
+
+def cosine_relation(tweet1, tweet2):
+    '''
+    This function calculates cosine similarity between two vectors
+    
+    It uses "numpy" package as "np"
+    
+    Input: tweet1, tweet2 as arrays of numbers
+    Output: float number - cosine similarity between tweet1 and tweet2
+    '''
+    return 0.5 * (1 + np.dot(tweet1, tweet2)/(np.linalg.norm(tweet1)*np.linalg.norm(tweet2)))
+
+def get_neigbours(test_vector, df_train_vectors, feature, k, text_column, class_column): 
+    '''
+    This function calculates k neirest neighbours to the test_vector using cosine similarity
+    
+    Input: test_vector - array of numbers
+           df_train_vectors - DataFrame with train instances
+           feature - name of a column in df_train_vectors with texts' embedding vectors
+           k - a number of neirest neighbours 
+           text_column - name of a column in df_train_vectors with texts
+           class_column - name of a column in df_train_vectors with texts' classes
+    Output: list of k neirest neighbours' texts and list with their classes
+    '''
+    distances = df_train_vectors[feature].apply(lambda x: cosine_relation(x, test_vector))
+    top_k = distances.sort_values(ascending=False)[:k]
+    df_top_k = df_train_vectors.loc[top_k.index]
+    return df_top_k[text_column].to_list(), df_top_k[class_column].to_list()
+    
+def weighted_knn(df, vector, class_vector, sample, K):   
+    '''
+    This function performs the weighted k neirest neighbours classification
+    
+    Input: df - DataFrame with train instances
+           vector - name of a column in df with texts' embedding vectors
+           class_vector - name of a column in df with texts' classes
+           sample - the test instance as array of numbers 
+           K - a number of neirest neighbours 
+           
+    Output: list of k neirest neighbours' texts and list with their classes
+    ''' 
+    class_range = list(set(df[class_vector].to_list()))
+    distances = df[vector].apply(lambda x: cosine_relation(x, sample))
+    top_k = distances.sort_values(ascending=False)[:K]
+    return np.argmax([sum(top_k[df.loc[top_k.index, class_vector] == i]) for i in class_range])
+    
+def cross_validation_ensemble_wknn(df, vector_name, class_vector, K_fold, k):
+    '''
+    This function performs cross-validation evaluate of the weighted k neirest neighbours classification
+    
+    Input: df - DataFrame with train instances
+           vector_name - name of a column in df with texts' embedding vectors
+           class_vector - name of a column in df with texts' classes
+           K_fold - number of folds of cross-validation
+           K - a number of neirest neighbours 
+           
+    Output: df as pandas DataFrame, identical to input df but with additional column 'Prediction' that contains predictions
+    ''' 
+    random_indices = np.random.permutation(df.index)
+    df['Prediction'] = None
+    for i in range(K_fold): 
+        # Split df on train and test data
+        test_data = df.loc[df.index.isin(random_indices[i*len(df.index)//K_fold:(i+1)*len(df.index)//K_fold])]
+        train_data = df[~df.index.isin(test_data.index)]
+        y = train_data[class_vector]
+        y_true = test_data[class_vector]  
+        # Apply wkNN
+        res = test_data[vector_name].apply(lambda x: weighted_knn(train_data, vector_name, x, k))
+        df['Prediction'].loc[test_data.index] = res
+    return df
--- code/tweets_embedding.py
+++ code/tweets_embedding.py
@@ -1,41 +1,29 @@
 from __future__ import print_function, division, unicode_literals
 import json
-# imports for torchmoji
-from torchmoji.sentence_tokenizer import SentenceTokenizer
-from torchmoji.model_def import torchmoji_feature_encoding
-from torchmoji.global_variables import PRETRAINED_PATH, VOCAB_PATH
-from transformers import AutoTokenizer, AutoModel, TFAutoModel
 import numpy as np
-from scipy.spatial.distance import cosine
-from collections import defaultdict
-# imports for roBERTa model
-from transformers import AutoModelForSequenceClassification
-from transformers import TFAutoModelForSequenceClassification
-from gensim.models import KeyedVectors
-import tensorflow_hub as hub
-from sentence_transformers import SentenceTransformer
 import torch
-from transformers import BertTokenizer, BertModel
-# Load pre-trained model tokenizer (vocabulary)
-tokenizer_bert = BertTokenizer.from_pretrained('bert-base-uncased')
-# Upload BERT model
-model_bert = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
-# path to the pre-loaded roBERTa model
-MODEL_path_roberta = r"../model/twitter-roberta-base-emotion"
-tokenizer_roberta = AutoTokenizer.from_pretrained(MODEL_path_roberta)
-# path to the pre-loaded Word2Vec model
-word2vec_model = KeyedVectors.load_word2vec_format('../model/GoogleNews-vectors-negative300.bin', binary=True)
-# uload the big Universal Sentence Encoder model from HTTPS domain
-model_use = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
-# upload Sentence-BERT model from the 'sentence_transformers' package
-model_sbert = SentenceTransformer('distilbert-base-nli-mean-tokens')
 
-
-def get_vector_bert(text):
+def get_vector_roberta(text, tokenizer_roberta, model_roberta):
     '''
-    This function provides BERT embedding for the tweet
-    Input: tweet as a string
-    Output: 768-dimentional vector as a list
+    This function provides Twitter-roBERTa-based embedding for the tweet.
+    
+    Input: tweet as a string, preloaded tokenizer_roberta and model_roberta
+    Output: 768-dimentional vector  
+    '''
+    encoded_input = tokenizer_roberta(text, return_tensors='tf')
+    features = model_roberta(encoded_input)
+    features = features[0].numpy()
+    features_mean = features[0][-1]
+    return features_mean
+    
+def get_vector_bert(text, tokenizer_bert, model_bert):
+    '''
+    This function provides BERT embedding for the tweet.
+    
+    It uses "torch" library to work with tensors.
+    
+    Input: tweet as a string, preloaded tokenizer_bert and model_bert
+    Output: 768-dimentional vector as 
     '''
     marked_text = "[CLS] " + text + " [SEP]"
     # Tokenize tweet with the BERT tokenizer
@@ -45,37 +33,34 @@ def get_vector_bert(text):
     # Convert inputs to PyTorch tensors
     tokens_tensor = torch.tensor([indexed_tokens])
     segments_tensors = torch.tensor([segments_ids])
-    # Make vector
+    # Make vector 
     with torch.no_grad():
-        _, _, hidden_states = model_bert(tokens_tensor, segments_tensors)
+        outputs = model_bert(tokens_tensor, segments_tensors)
+        hidden_states = outputs[2]
     token_vecs = hidden_states[-2][0]
     sentence_embedding = torch.mean(token_vecs, dim=0)
     return sentence_embedding.cpu().detach().numpy()
-
-
-def get_vector_roberta(text):
+    
+def get_vector_sbert(tweet, model_sbert):
     '''
-    This function provides Twitter-roBERTa-based embedding for the tweet
-    Input: tweet as a string
-    Output: 768-dimentional vector as a list
+    This function provides Sentence-BERT embedding for the tweet.
+    
+    Input: tweet as a string, preloaded model_sbert
+    Output: 768-dimentional vector as a list 
     '''
-    encoded_input = tokenizer_roberta(text, return_tensors='tf')
-    model = TFAutoModel.from_pretrained(MODEL_path_roberta)
-    features = model(encoded_input)
-    features = features[0].numpy()
-    features_mean = np.mean(features[0], axis=0)
-    return features_mean
+    return model_sbert.encode(tweet)
 
-
-def get_vectors_deepmoji(data, tweets_column):
+def get_vectors_deepmoji(tweets):
     '''
-    This function provides DeepMoji (torchmoji) embedding for the tweet
-    Input: tweet as a string
-    Output: 2304-dimentional vector as a list
+    This function provides DeepMoji (torchmoji) embedding for the tweet.
+    
+    It uses "json" library to upload DeepMoji vocabulary.
+    
+    Input: tweets - a list of strings
+    Output: 2304-dimentional vector as a list 
     '''
     maxlen = 30
     batch_size = 32
-    tweets = list(data[tweets_column])
     with open(VOCAB_PATH, 'r') as f:
         vocabulary = json.load(f)
     st = SentenceTokenizer(vocabulary, maxlen)
@@ -84,40 +69,35 @@ def get_vectors_deepmoji(data, tweets_column):
     encoding = model(tokenized)
     vectors = []
     for i in range(len(encoding)):
-        vectors.append(encoding[i, :])
+        vectors.append(encoding[i,:])
     return vectors
-
-
-def get_vector_w2v(tweet):
+    
+def get_vector_use(tweet, model_use):
     '''
-    This function provides Word2Vec embedding for the tweet
-    Input: Word2Vec model imported with KeyedVectors, tweet as a string
-    Output: 300-dimentional vector as a list
-    '''
-    vectors = []
-    for w in tweet.split():
-        if w in word2vec_model.wv.vocab:
-            vectors.append(word2vec_model.wv.__getitem__(w))
-        else:
-            vectors.append(np.zeros(300))
-    return np.mean(vectors, axis=0)
-
-
-def get_vector_use(tweet):
-    '''
-    This function provides Universal Sentence Encoder embedding for the tweet
-    Input: tweet as a string
-    Output: 512-dimentional vector as a list
+    This function provides Universal Sentence Encoder embedding for the tweet.
+    
+    It uses "numpy" library as "np" to process vectors.
+    
+    Input: tweet as a string, preloaded model_use
+    Output: 512-dimentional vector as a list 
     '''
     message_embeddings = model_use([tweet])
     mess_list = np.array(message_embeddings).tolist()
     return mess_list[0]
-
-
-def get_vector_sbert(tweet):
+    
+def get_vector_w2v(tweet, w2v_model):
     '''
-    This function provides Sentence-BERT embedding for the tweet
-    Input: tweet as a string
-    Output: 768-dimentional vector as a list
+    This function provides Word2Vec embedding for the tweet.
+    
+    It uses "numpy" library as "np" to create zeros vectors.
+    
+    Input: tweet as a string, preloaded Word2Vec model
+    Output: 300-dimentional vector as a list 
     '''
-    return model_sbert.encode(tweet)
+    vectors = []
+    for w in tweet.split():
+        if w in w2v_model.wv.vocab:
+            vectors.append(w2v_model.wv.__getitem__(w))
+        else:
+            vectors.append(np.zeros(300))
+    return np.mean(vectors, axis = 0)
